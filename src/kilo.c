3f94b57e6144ba55d1bc7b38cf40a253bcda6fcd
anand <posidon254@gmailcom>
2025-08-24 11:40:51 +0530

    some minor changes

--- src/kilo.c
+++ src/kilo.c
@@ -1,8 +1,26 @@
-#include <stdio.h>
+//#define _POSIX_C_SOURCE 200112L
+//#define _DARWIN_C_SOURCE
+
+
 #include <unistd.h>
+#include <termios.h>
+#include <stdio.h>
 
-int main() {
+void enableRawMode(void) {
+	struct termios raw ;
 
+//stdin_flo not working , i am not able to fetch the valure of this variable
+	
+	tcgetattr(STDIN_FLENO , &raw );
+
+	raw.c_lflag &= ~(ECHO) ;
+
+	tcsetattr(STDIN_FILENO , TCSAFLUSH , &raw );
+}
+
+int main(void) {
+	
+	enableRawMode() ;
 	char c;
 	while ( read(STDIN_FILENO ,&c , 1 ) == 1 && c != 'q' ) ;
 	return 0 ;
